6ec2a314360d32a7d29cd19d27c42e8b8eaedc8e
YongShinn <45961196+YongShinn@users.noreply.github.com>
2019-01-02 13:09:01 +0800

    V.4

--- landing_Page.js
+++ landing_Page.js
@@ -6,15 +6,17 @@ const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 const app = express();
 app.use(bodyParser.json()); // to support JSON bodie
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/", function(req, res){
+app.post("/", function(req, res) {
   var nam = req.body.in_Name;
   var mail = req.body.in_Email;
   console.log(nam + mail);
@@ -22,13 +24,16 @@ app.post("/", function(req, res){
   var xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://l3f3ds1nn1.execute-api.ap-southeast-1.amazonaws.com/deploy/landing-page');
   xhr.onreadystatechange = function(event) {
-    if (xhr.readyState === 4)
-    {
+    if (xhr.readyState === 4) {
       console.log(xhr.responseText);
     }
   };
   xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.send(JSON.stringify({name: nam, email: mail, gender: sex}));
+  xhr.send(JSON.stringify({
+    name: nam,
+    email: mail,
+    gender: sex
+  }));
   res.redirect("/#interest");
 });
 
